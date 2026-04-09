6f0808f43c75125a1f634a87989fd3c1d21ce562
Chittaranjan Panda <chittaranjanpanda.work@gmail.com>
2026-04-09 15:45:47 +0530

    added admin initializer

---
+++ src/main/java/com/hms/hospitalmanagement/config/DataInitializer.java
@@ -0,0 +1,31 @@
+package com.hms.hospitalmanagement.config;
+
+import com.hms.hospitalmanagement.entity.User;
+import com.hms.hospitalmanagement.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class DataInitializer {
+
+    @Bean
+    CommandLineRunner initAdmin(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+
+            if (userRepository.findByEmail("chittaraja16@gmail.com").isEmpty()) {
+
+                User admin = new User();
+                admin.setName("Admin");
+                admin.setEmail("chittaraja16@gmail.com");
+                admin.setPassword(passwordEncoder.encode("Chitta@1615"));
+                admin.setRole("ROLE_ADMIN");
+
+                userRepository.save(admin);
+
+                System.out.println("✅ Admin created!");
+            }
+        };
+    }
+}
