d2963d75c35bcfb0d95961d7a2814f076e0e9b54
Chittaranjan Panda <chittaranjanpanda.work@gmail.com>
2026-04-09 19:10:40 +0530

    fix startup crash remove jwt and fix db

--- src/main/java/com/hms/hospitalmanagement/config/SecurityConfig.java
+++ src/main/java/com/hms/hospitalmanagement/config/SecurityConfig.java
@@ -1,12 +1,10 @@
 package com.hms.hospitalmanagement.config;
 
-import com.hms.hospitalmanagement.security.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,21 +12,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig {
 
-    private final JwtFilter jwtFilter;
-
-    public SecurityConfig(JwtFilter jwtFilter) {
-        this.jwtFilter = jwtFilter;
-    }
-
-    // 🔐 Password encoder (REQUIRED)
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    // 🔐 Security configuration
     @Bean
-    
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http
